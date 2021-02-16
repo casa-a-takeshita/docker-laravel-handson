3a1959af4ab6a43f4188328854bea383909cc8da
asuka_takeshita <asuka.takeshita@casa-inc.co.jp>
2021-02-17 07:56:43 +0900

    ToDoアプリの認証機能を作る パート2（パスワード再設定箇所に不具合あり）

--- backend/app/Models/User.php
+++ backend/app/Models/User.php
@@ -7,6 +7,9 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
+use App\Mail\ResetPassword;
+use Illuminate\Support\Facades\Mail;
+
 class User extends Authenticatable
 {
     use HasFactory, Notifiable;
@@ -40,6 +43,11 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function sendPasswordResetNotification($token)
+    {
+        Mail::to($this)->send(new ResetPassword($token));
+    }
 
     public function folders()
     {
