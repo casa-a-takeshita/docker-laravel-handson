204d52c99db850e152a620d764c3dd432e11f969
asuka_takeshita <asuka.takeshita@casa-inc.co.jp>
2021-02-16 12:15:14 +0900

    ToDoアプリの認証機能を作る パート1

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -17,6 +17,10 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Auth::routes();
+
+Route::get('/', 'App\Http\Controllers\HomeController@index')->name('home');
+
 Route::get('/folders/{id}/tasks', 'App\Http\Controllers\TaskController@index')->name('tasks.index');
 
 Route::get('/folders/create', 'App\Http\Controllers\FolderController@showCreateForm')->name('folders.create');
@@ -27,3 +31,7 @@ Route::post('/folders/{id}/tasks/create', 'App\Http\Controllers\TaskController@c
 
 Route::get('/folders/{id}/tasks/{task_id}/edit', 'App\Http\Controllers\TaskController@showEditForm')->name('tasks.edit');
 Route::post('/folders/{id}/tasks/{task_id}/edit', 'App\Http\Controllers\TaskController@edit');
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
